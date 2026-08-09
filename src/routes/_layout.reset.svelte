cd49b8588bc166bf211edd4b3c8c59a2a2389d8b
sankalpdevtester <ohofufic0@gmail.com>
2026-08-09 14:17:41 +0530

    feat: add layout reset component

---
+++ src/routes/_layout.reset.svelte
@@ -0,0 +1,30 @@
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(body) {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+  }
+
+  :global(h1, h2, h3, h4, h5, h6) {
+    font-weight: bold;
+    margin-bottom: 0.5em;
+  }
+
+  :global(p) {
+    margin-bottom: 1em;
+  }
+
+  :global(a) {
+    text-decoration: none;
+    color: #337ab7;
+  }
+
+  :global(a:hover) {
+    color: #23527c;
+  }
+</style>
